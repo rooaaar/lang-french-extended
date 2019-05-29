d4de76f9f1c9a8744b25241d8c9b62912e22029c
hiobi <hiob.bjorn@gmail.com>
2019-05-29 18:47:52 +0200

    Add Same-Yaml scripts for FoF

---
+++ scripts/identical/fof-default-group.sh
@@ -0,0 +1,46 @@
+VERSION='master' #Repository version
+REPO='FriendsOfFlarum/default-group' #Repository name
+LOCALE='resources/locale' #Locale folder path
+
+YAML1='en.yml' #Original yaml file
+YAML2='fof-default-group.yml' #Translated yaml file
+
+TEMP_DIR=`mktemp -d`
+WORK_DIR=`pwd`
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+if ! [ -x "$(command -v same-yaml)" ]; then
+  echo 'Error: same-yaml is not installed.' >&2
+  exit 1
+fi
+
+if [[ ! "$TEMP_DIR" || ! -d "$TEMP_DIR" ]]; then
+  exit 1
+fi
+
+function cleanup {
+  rm -rf "$TEMP_DIR"
+}
+
+cd "$TEMP_DIR"
+
+curl -s -L "https://raw.githubusercontent.com/$REPO/$VERSION/$LOCALE/$YAML1" > $YAML1
+
+RC=0
+
+  echo "Testing $YAML1 against $YAML2:"
+  same-yaml --ref "$YAML1" --tra "$WORK_DIR/locale/$YAML2"
+  if [ $YAML1 = $YAML2 ]
+  then
+    RC=1
+    printf "${RED}⨉ failed${NC}\n"
+  else
+    printf "${GREEN}✓ passed${NC}\n"
+  fi
+
+trap cleanup EXIT
+
+exit $RC
