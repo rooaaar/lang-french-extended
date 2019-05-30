b9a346004d86eb253ef8e5f1ad17d9dedac7ea47
hiob <Hiobi@users.noreply.github.com>
2019-05-30 18:38:23 +0200

    Update changelog.sh

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -4,5 +4,4 @@ git config --global user.name "Travis CI"
 git add "CHANGELOG.md"
 git commit --message "auto-generated CHANGELOG.md"
 
-git remote add origin https://github.com/rooaaar/lang-french-extended
 git push --quiet --set-upstream origin
