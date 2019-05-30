fff2130ab32fac39b9e6d81c6a21011a67156fea
hiob <Hiobi@users.noreply.github.com>
2019-05-30 17:38:02 +0200

    Update changelog.sh

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -5,4 +5,4 @@ git config user.email "github@travis-ci.org" && \
 
 git add "CHANGELOG.md" && \
 git commit -m "Auto-Generating CHANGELOG.md" && \
-git push master:rooaaar/lang-french-extended HEAD:master
+git push https://github.com/rooaaar/lang-french-extended.git rooaaar/lang-french-extended
