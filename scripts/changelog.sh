467d947ca7dcaa7ea9e8f8c689a379fddf4052a6
hiob <Hiobi@users.noreply.github.com>
2019-05-30 18:02:05 +0200

    git push --force

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -2,5 +2,5 @@ git config user.name "Travis CI" && \
 git config user.email "github@travis-ci.org" && \
 
 git add "CHANGELOG.md" && \
-git commit -m "Auto-Generating CHANGELOG.md" && \
-git push https://github.com/rooaaar/lang-french-extended.git rooaaar/lang-french-extended
+git commit -m "auto-generated CHANGELOG.md" && \
+git push --force
