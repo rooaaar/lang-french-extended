625269eb8da2f4b4ed94507dad88dd7aa7822969
hiob <Hiobi@users.noreply.github.com>
2019-05-30 19:28:22 +0200

    Update changelog.sh

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,6 +1,8 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
+git branch 
+
 git add "CHANGELOG.md"
 git commit --message "auto-generated CHANGELOG.md"
-git push --quiet origin master:master
+git push -u --quiet origin master:master
