94b32caaec05d7358b63e9b76ba59bc1704ebad6
hiob <Hiobi@users.noreply.github.com>
2019-05-30 22:02:03 +0200

    Update changelog.sh

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -22,7 +22,7 @@ upload_files() {
   # Remove existing "origin"
   git remote rm origin
   # Add new "origin" with access token in the git URL for authentication
-  git remote add origin https://rooaaar:${GH_TOKEN}@github.com/rooaaar/lang-french-extended.git > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/rooaaar/lang-french-extended.git > /dev/null 2>&1
   git push origin master --quiet
 }
 
