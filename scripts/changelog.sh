b9c8677aef4dc4568989e9327832ef94987e3860
hiob <Hiobi@users.noreply.github.com>
2019-05-30 21:32:30 +0200

    Test @vinaygopinath script
    https://www.vinaygopinath.me/blog/tech/commit-to-master-branch-on-github-using-travis-ci/

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -1,8 +1,39 @@
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+#!/bin/sh
+# Credit: https://gist.github.com/willprice/e07efd73fb7f13f917ea
 
-git branch 
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
 
-git add "CHANGELOG.md"
-git commit --message "auto-generated CHANGELOG.md"
-git push -u --quiet github.com:rooaaar/lang-french-extended HEAD:master
+commit_files() {
+  git checkout master
+  # Current month and year, e.g: Apr 2018
+  dateAndMonth=`date "+%b %Y"`
+  # Stage the modified files in dist/output
+  git add "CHANGELOG.md"
+  # Create a new commit with a custom build message
+  # with "[skip ci]" to avoid a build loop
+  # and Travis build number for reference
+  git commit -m "Travis update: $dateAndMonth (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
+}
+
+upload_files() {
+  # Remove existing "origin"
+  git remote rm origin
+  # Add new "origin" with access token in the git URL for authentication
+  git remote add origin https://rooaaar:${GH_TOKEN}@github.com/rooaaar/lang-french-extended.git > /dev/null 2>&1
+  git push origin master --quiet
+}
+
+setup_git
+
+commit_files
+
+# Attempt to commit to git only if "git commit" succeeded
+if [ $? -eq 0 ]; then
+  echo "A new commit in CHANGELOG.md. Uploading to GitHub"
+  upload_files
+else
+  echo "No changes in CHANGELOG.md. Nothing to do."
+fi
