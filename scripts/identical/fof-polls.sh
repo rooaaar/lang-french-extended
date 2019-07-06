eeaaa257b7bf13209e734c79c37963734ad12f77
Travis CI <github@travis-ci.org>
2019-07-06 23:21:00 +0200

    Move to FoF Namespace

--- scripts/identical/fof-polls.sh
+++ scripts/identical/fof-polls.sh
@@ -1,9 +1,9 @@
 VERSION='master' #Repository version
-REPO='ReFlar/polls' #Repository name
+REPO='FriendsOfFlarum/polls' #Repository name
 LOCALE='resources/locale' #Locale folder path
 
 YAML1='en.yml' #Original yaml file
-YAML2='reflar-polls.yml' #Translated yaml file
+YAML2='fof-polls.yml' #Translated yaml file
 
 TEMP_DIR=`mktemp -d`
 WORK_DIR=`pwd`
